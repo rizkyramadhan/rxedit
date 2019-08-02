d6fb941e8c104c021fc451604ed6a870f5bf21da
rizky <rizky@andromedia.co.id>
2019-08-02 10:03:44 +0700

    fix

--- backend/src/parser.ts
+++ backend/src/parser.ts
@@ -1,5 +1,5 @@
-import { SyntaxKind, SourceFile } from "ts-morph";
-import * as _ from "lodash";
+import { SyntaxKind, SourceFile } from 'ts-morph';
+import * as _ from 'lodash';
 
 function getKindNamesForApi() {
   // some SyntaxKinds are repeated, so only use the first one
@@ -14,7 +14,7 @@ function getKindNamesForApi() {
 }
 export const kindNames = getKindNamesForApi();
 
-export const parseTag = (e: any) => {
+export const parseTag = (e: any): any => {
   const node = e._compilerNode || e;
   let kind = e.kind;
   if (!kind && e.getKind) {
@@ -33,7 +33,7 @@ export const parseTag = (e: any) => {
       };
     case SyntaxKind.JsxElement:
       return {
-        tag: _.get(node, "openingElement.tagName.escapedText"),
+        tag: _.get(node, 'openingElement.tagName.escapedText'),
         props: _.map(node.openingElement.attributes.properties, i =>
           parseProp(i)
         ),
@@ -47,11 +47,11 @@ export const parseTag = (e: any) => {
 
 const parseProp = (e: any) => {
   const kind = kindNames[e.initializer.expression.kind];
-  const detail = [];
+  const detail: any[] = [];
 
-  if (kind === "ObjectLiteralExpression") {
+  if (kind === 'ObjectLiteralExpression') {
     if (e.initializer.expression.properties) {
-      e.initializer.expression.properties.map(p => {
+      e.initializer.expression.properties.map((p: any) => {
         if (p.identifier) {
           detail[p.identifier.escapedText] = {
             kind: kind[p.initializer.kind],
@@ -79,13 +79,13 @@ export const getImport = (sf: SourceFile) => {
     const ic = i.getImportClause();
     const ms = i.getModuleSpecifier();
     const nb = i.getNamedImports();
-    
+
     return {
-      default: _.get(ic, "_compilerNode.name.escapedText"),
+      default: _.get(ic, '_compilerNode.name.escapedText'),
       named: _.map(nb, n => {
         return n.getText();
       }),
-      from: _.get(ms, "_compilerNode.text")
+      from: _.get(ms, '_compilerNode.text')
     };
   });
 };
