cd67bd135c3d75d134f88a780ee44f9975954a70
rizky <rizky@andromedia.co.id>
2019-08-05 11:28:42 +0700

    fox

---
+++ app/src/Comp206685.tsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+import { View } from "react-xp";
+
+export default () => {
+  return (<View/>);
+};
+console.log(5);
+console.log(6);
--- frontend/src/components/Project/Start.tsx
+++ frontend/src/components/Project/Start.tsx
@@ -1,19 +1,19 @@
-import _ from "lodash";
-import { observer, useObservable } from "mobx-react-lite";
-import { Label, Link, PrimaryButton, TextField } from "office-ui-fabric-react";
-import React from "react";
+import _ from 'lodash';
+import { observer, useObservable } from 'mobx-react-lite';
+import { Label, Link, PrimaryButton, TextField } from 'office-ui-fabric-react';
+import React from 'react';
 
 export default observer(({ data }: any) => {
   data = useObservable({
-    path: "./app"
+    path: '../app/src/'
   });
   return (
     <div
       style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "absolute",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
         top: 0,
         left: 0,
         right: 0,
@@ -25,23 +25,24 @@ export default observer(({ data }: any) => {
         <TextField
           defaultValue={data.path}
           onChange={e => {
-            let val = _.get(e, "nativeEvent.target.value");
+            let val = _.get(e, 'nativeEvent.target.value');
             data.path = val;
           }}
         />
         <div
           style={{
             marginTop: 10,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between"
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
           }}
         >
           <PrimaryButton
             onClick={e => {
-              window.localStorage["rx-edit-path"] = data.path;
+              window.localStorage['rx-edit-path'] = data.path;
+              window.location.reload();
             }}
-            text="Submit"
+            text='Submit'
             style={{ marginRight: 10 }}
           />
           <div>
